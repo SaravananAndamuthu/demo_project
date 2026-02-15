73f9ddbc1cbb89800aa501b5be0aeb004b6ffaaa
SaravananAndamuthu <142229775+SaravananAndamuthu@users.noreply.github.com>
2026-02-15 18:49:52 +0530

    Add files via upload

---
+++ components/Stats.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import CountUp from "react-countup"
+
+const stats = [
+    {num: 0 , text: 'Year of Experiance'},
+    {num: 2, text: 'Project Completed'},
+    {num: 4, text: "Technology Mastered"},
+    {num: 10, text: "Code Commits"}
+]
+
+const Stats = () => {
+  return (
+    <section className="pt-4 pb-12 xl:pb-0 xl:pt-0">
+        <div className="container mx-auto">
+            <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:mx-w-none">
+                {stats.map((item, index)=>{
+                    return (
+                        <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
+                            <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold"/>
+                            <p className={`${item.text.length < 15 ? "max-w-[100px]": "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Stats
